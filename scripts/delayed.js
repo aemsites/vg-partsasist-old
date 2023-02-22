845ed849e260c59fa5a9afdcf15db5ceb89a7955
Aravindo Wingeier <wingeier@adobe.com>
2023-02-22 14:03:50 -0800

    load OneTrust Cookies

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,31 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
 
+const loadScript = (url, attrs) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
+    }
+  }
+  head.append(script);
+  return script;
+};
+
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// OneTrust Cookies Consent Notice start
+loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
+  type: 'text/javascript',
+  charset: 'UTF-8',
+  'data-domain-script': '590e5ab0-645c-4477-8464-061e57925731',
+});
+
+window.OptanonWrapper = () => {};
+// OneTrust Cookies Consent Notice end
