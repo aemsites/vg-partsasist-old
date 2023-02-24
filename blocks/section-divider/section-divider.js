01aea7b15cece57694d77b19e78c872e9cf22e5d
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2023-02-24 15:40:18 +0100

    #8 - add section divider block

---
+++ blocks/section-divider/section-divider.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  const [header, subheader] = block.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  const desciption = block.querySelectorAll('h1 + p, h2 + p, h3 + p, h4 + p, h5 + p, h6 + p');
+
+  if (header) {
+    header.classList.add('section-divider--header');
+    console.log(header);
+  }
+
+  if (subheader) {
+    subheader.classList.add('section-divider--subheader');
+    console.log(subheader);
+  }
+
+  if (desciption.length) {
+    const descriptionWrapper = document.createElement('div');
+
+    descriptionWrapper.append(...desciption);
+    block.appendChild(descriptionWrapper);
+  }
+}
