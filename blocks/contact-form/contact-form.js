f1835b2484c96f1755f627472b3fd718208f7ed4
Darin Kuntze <dkuntze@gmail.com>
2023-02-25 16:01:18 -0600

    wip 2 form

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,8 +1,38 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
+function addRequiredDiv(fieldName) {
+  const requiredNode = document.createElement('div');
+  requiredNode.innerText = fieldName.includes('email') ? 'A valid email address is required' : 'This field is required';
+  requiredNode.classList.add('required-field');
+  const pDiv = fieldName.includes('paragraphText') ? document.querySelector(`#contact textarea[name=${fieldName}]`).parentNode : document.querySelector(`#contact input[name=${fieldName}]`).parentNode;
+  pDiv.insertAdjacentElement('afterend', requiredNode);
+}
 function validateForm(event) {
+  const emailRegex = /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
   event.preventDefault();
-  console.log(event);
+  if (document.querySelector('#contact input[name="firstName"]').value === '') {
+    addRequiredDiv('firstName');
+  }
+
+  if (document.querySelector('#contact input[name="lastName"]').value === '') {
+    addRequiredDiv('lastName');
+  }
+
+  if (document.querySelector('#contact input[name="email-address"]').value === '' || !emailRegex.test(document.querySelector('#contact input[name="email-address"]').value)) {
+    addRequiredDiv('email-address');
+  }
+
+  if (document.querySelector('#contact textarea[name="paragraphText"]').value === '') {
+    addRequiredDiv('paragraphText');
+  }
+}
+
+function fieldFocus(event) {
+  const pDiv = event.currentTarget.parentNode.parentNode;
+  if (pDiv.children.length > 2) {
+    pDiv.children[2].remove();
+  }
+  console.log(pDiv.children.length);
 }
 export default async function decorate(block) {
   const config = readBlockConfig(block);
@@ -36,5 +66,12 @@ export default async function decorate(block) {
       + '</div></form>';
 
   const form = block.querySelector('#contact');
+  const inputs = block.querySelectorAll('#contact input');
+  inputs.forEach((input) => {
+    input.addEventListener('focus', fieldFocus);
+  });
+
+  const ta = block.querySelector('#contact textarea');
+  ta.addEventListener('focus', fieldFocus);
   form.addEventListener('submit', validateForm);
 }
