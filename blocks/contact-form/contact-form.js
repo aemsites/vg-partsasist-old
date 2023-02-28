f30f279fb295fdb8e5c7ea66e753bbf9c7d96c6e
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-02-28 11:24:12 +0100

    #5 contact form updated and styled

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,10 +1,17 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
+function getFormElement(fieldName, type = 'input') {
+  return document.querySelector(`#contact ${type}[name=${fieldName}]`);
+}
+
 function addRequiredDiv(fieldName) {
   const requiredNode = document.createElement('div');
-  requiredNode.innerText = fieldName.includes('email') ? 'A valid email address is required' : 'This field is required';
+  requiredNode.innerText = fieldName.includes('email')
+    ? 'A valid email address is required'
+    : 'This field is required';
   requiredNode.classList.add('required-field');
-  const pDiv = fieldName.includes('paragraphText') ? document.querySelector(`#contact textarea[name=${fieldName}]`).parentNode : document.querySelector(`#contact input[name=${fieldName}]`).parentNode;
+  const fieldType = fieldName.includes('paragraph-text') ? 'textarea' : 'input';
+  const pDiv = getFormElement(fieldName, fieldType).parentNode;
   if (pDiv.parentNode.children.length === 2) {
     pDiv.insertAdjacentElement('afterend', requiredNode);
   }
@@ -12,13 +19,14 @@ function addRequiredDiv(fieldName) {
 
 async function submitForm(form, endpoint) {
   const payload = {};
-  payload.firstName = document.querySelector('#contact input[name="firstName"]').value;
-  payload.lastName = document.querySelector('#contact input[name="lastName"]').value;
-  const dc = document.querySelector('#contact select[name="dealer-customer"]');
-  payload.dealerCustomer = dc.options[dc.selectedIndex].text;
-  payload.phone = document.querySelector('#contact input[name="phone"]').value;
-  payload.emailAddress = document.querySelector('#contact input[name="email-address"]').value;
-  payload.paragraphText = document.querySelector('#contact textarea[name="paragraphText"]').value;
+  const formFields = [...form];
+  formFields.forEach((ff) => {
+    if (ff.localName === 'button') return;
+    const name = ff.name.includes('-')
+      ? ff.name.replace(/-([a-z])/g, (g) => g[1].toUpperCase())
+      : ff.name;
+    payload[name] = ff.localName === 'select' ? ff.options[ff.selectedIndex].value : ff.value;
+  });
   const action = endpoint.trim();
   const resp = await fetch(action, {
     method: 'POST',
@@ -41,32 +49,32 @@ async function submitForm(form, endpoint) {
   }
 }
 
-async function validateForm(event, endpoint) {
+async function validateForm(event, endpoint, form) {
   let valid = true;
+  const configForm = {
+    firstName: { required: true },
+    lastName: { required: true },
+    'email-address': { required: true, emailPattern: true },
+    'paragraph-text': { required: true, type: 'textarea' },
+  };
   const emailRegex = /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
   event.preventDefault();
-  if (document.querySelector('#contact input[name="firstName"]').value === '') {
-    addRequiredDiv('firstName');
-    valid = false;
-  }
 
-  if (document.querySelector('#contact input[name="lastName"]').value === '') {
-    addRequiredDiv('lastName');
-    valid = false;
-  }
+  Object.keys(configForm).forEach((fieldName) => {
+    const type = configForm[fieldName].type ?? 'input';
+    const { value } = getFormElement(fieldName, type);
+    const { required, emailPattern } = configForm[fieldName];
+    const isEmpty = required && value === '';
+    const notEmail = emailPattern && !emailRegex.test(value);
 
-  if (document.querySelector('#contact input[name="email-address"]').value === '' || !emailRegex.test(document.querySelector('#contact input[name="email-address"]').value)) {
-    addRequiredDiv('email-address');
-    valid = false;
-  }
-
-  if (document.querySelector('#contact textarea[name="paragraphText"]').value === '') {
-    addRequiredDiv('paragraphText');
-    valid = false;
-  }
+    if (isEmpty || notEmail) {
+      addRequiredDiv(fieldName);
+      valid = false;
+    }
+  });
 
   if (valid) {
-    await submitForm(document.querySelector('#contact'), endpoint);
+    await submitForm(form, endpoint);
   }
 }
 
@@ -76,35 +84,52 @@ function fieldFocus(event) {
     pDiv.children[2].remove();
   }
 }
+
+/**
+ * decorate the contact form
+ * @param {HTMLElement} block form element
+ */
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-  block.innerHTML = '<form id="contact"><div class="form-element col-left">'
-      + '<div>First Name</div>'
-      + '<div><input type="text" name="firstName"/></div>'
-      + '</div>'
-      + '<div class="form-element col-right">'
-      + '<div>Last Name</div>'
-      + '<div><input type="text" name="lastName"/></div>'
-      + '</div>'
-      + '<div class="form-element picker">'
-      + '<div>Are you a dealer or a customer?</div>'
-      + '<div><select name="dealer-customer"><option value="dealer">Dealer</option><option value="customer">Customer</option><option value="other">Other</option></select></div>'
-      + '</div>'
-      + '<div class="form-element">'
-      + '<div>Phone</div>'
-      + '<div><input type="text" name="phone"/></div>'
-      + '</div>'
-      + '<div class="form-element">'
-      + '<div>Email Address</div>'
-      + '<div><input type="text" name="email-address"/></div>'
-      + '</div>'
-      + '<div class="form-element question">'
-      + '<div>How can we help you?</div>'
-      + '<div><textarea name="paragraphText"></textarea></div>'
-      + '</div>'
-      + '<div class="form-element">'
-      + '<div><input type="submit" value="Submit"/></div>'
-      + '</div></form>';
+  block.innerHTML = `
+  <form id="contact">
+    <div class="row">
+      <div class="form-element col-left">
+        <div class="required">First Name</div>
+        <div><input type="text" name="firstName"/></div>
+      </div>
+      <div class="form-element col-right">
+        <div class="required">Last Name</div>
+        <div><input type="text" name="lastName"/></div>
+      </div>
+    </div>
+    <div class="form-element picker">
+      <div>Are you a dealer or a customer?</div>
+      <div>
+        <select name="dealer-customer">
+          <option value="Dealer">Dealer</option>
+          <option value="Customer">Customer</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-element">
+      <div>Phone</div>
+      <div><input type="text" name="phone"/></div>
+    </div>
+    <div class="form-element">
+      <div class="required">Email Address</div>
+      <div><input type="text" name="email-address"/></div>
+    </div>
+    <div class="form-element question">
+      <div class="required">How can we help you?</div>
+      <div><textarea name="paragraph-text"></textarea></div>
+    </div>
+    <div class="form-element">
+      <div><button type="submit"/>Submit</button></div>
+    </div>
+  </form>
+  `;
 
   const form = block.querySelector('#contact');
   const inputs = block.querySelectorAll('#contact input');
@@ -114,5 +139,5 @@ export default async function decorate(block) {
 
   const ta = block.querySelector('#contact textarea');
   ta.addEventListener('focus', fieldFocus);
-  form.addEventListener('submit', (event) => validateForm(event, config.endpoint));
+  form.addEventListener('submit', (event) => validateForm(event, config.endpoint, form));
 }
