fcaa791c97aa39f21a9fedb342e2d8aadb76d065
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-02-28 16:07:55 +0100

    #24 heromain doesn't overlap over the next section

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,18 @@
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
+  const config = readBlockConfig(block);
+  // setup metadata and remove all of them from the DOM
+  if (config && Object.keys(config).length === 1) {
+    const configKey = block.children[1].children[0];
+    const configValue = block.children[1].children[1];
+    const [key, value] = Object.entries(config)[0];
+    const isKey = configKey.innerText === key;
+    const isValue = configValue.innerText === value;
+    if (isKey && isValue) block.children[1].remove();
+    block.dataset[key] = value;
+    if (key === 'id') block.id = value;
+  }
   /* Move all buttons to one container */
   const firstButtonContainer = block.querySelector('.button-container');
   if (firstButtonContainer) {
