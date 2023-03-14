1059e2f5d7192ff548108214ced72d667ee4f9f1
Aravindo Wingeier <wingeier@adobe.com>
2023-03-14 10:00:53 -0700

    on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,  because the cookie is not persistent. To avoid this annoyance, disable unless on the  production page.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -33,11 +33,16 @@ const fireGTM = (w, d, s, l, i) => {
 };
 
 // OneTrust Cookies Consent Notice start
-loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
-  type: 'text/javascript',
-  charset: 'UTF-8',
-  'data-domain-script': '078b3e5b-9ab3-4e0f-99e4-1d0b7ff41e53',
-});
+if (window.location.host.includes('partsasist.com')) {
+  // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
+  // because the cookie is not persistent. To avoid this annoyance, disable unless on the
+  // production page.
+  loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
+    type: 'text/javascript',
+    charset: 'UTF-8',
+    'data-domain-script': '078b3e5b-9ab3-4e0f-99e4-1d0b7ff41e53',
+  });
+}
 
 fireGTM(window, document, 'script', 'dataLayer', 'GTM-NRP6TBV');
 window.OptanonWrapper = () => {};
