5687184ff6a9a8548b6d5d4d44c89b7d9915e608
Manuel Vara <manuel.vara@netcentric.biz>
2023-11-14 13:35:20 +0100

    Move loadScript function

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,20 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
 
-const loadScript = (url, attrs) => {
-  const head = document.querySelector('head');
-  const script = document.createElement('script');
-  script.src = url;
-  if (attrs) {
-    // eslint-disable-next-line no-restricted-syntax, guard-for-in
-    for (const attr in attrs) {
-      script.setAttribute(attr, attrs[attr]);
-    }
-  }
-  head.append(script);
-  return script;
-};
-
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
